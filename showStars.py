06546177fa9a2542fb445dc8e493a6f867d7920a
Anthony Sim <31682285+anthonysim@users.noreply.github.com>
2019-11-29 14:39:56 -0800

    Add files via upload

---
+++ showStars.py
@@ -0,0 +1,10 @@
+# A function that forms a "tree" based on
+# the amount of rows specified.
+
+def showStars(row):
+
+    for i in range(1, row + 1):
+        stars =  '*' * i
+        print(stars)
+        
+showStars(5)
