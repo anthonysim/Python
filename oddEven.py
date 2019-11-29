52adf68cd97a7af6e9ea70432f5d85cc6ab4ee79
Anthony Sim <31682285+anthonysim@users.noreply.github.com>
2019-11-29 11:09:58 -0800

    Update oddEven.py

--- oddEven.py
+++ oddEven.py
@@ -5,8 +5,8 @@ a function that iterates through, while specifying
 what number of the iteration is odd or even.
 */
 
-def showNumber(number):
-    for i in range(number + 1):
+def showNumber(limit):
+    for i in range(limit + 1):
         if i % 2 == 0:
             print(str(i) + " EVEN")
         else:
