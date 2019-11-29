191169d43e62a9f08d6ac04c30bc3756a2198bc5
Anthony Sim <31682285+anthonysim@users.noreply.github.com>
2019-11-29 13:11:13 -0800

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,14 @@
+# A function that adds up the total for
+# multiples of 5 and 3.
+
+# Number limit is set within the function.
+
+def sum(limit):
+    total = 0
+    for i in range(1, limit +1):
+        if i % 5 == 0 or i % 3 == 0:
+            total += i
+
+    return total
+
+print(sum(10))
